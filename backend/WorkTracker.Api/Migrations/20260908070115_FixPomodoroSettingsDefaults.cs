0d7a6fb1d2318a3b7b68a4bbf0e128c1cb534648
Kagan Cubukcu <kagancancubukcu@gmail.com>
2026-09-08 10:25:52 +0300

    fix: backfill zeroed pomodoro settings from a prior migration bug

---
+++ backend/WorkTracker.Api/Migrations/20260908070115_FixPomodoroSettingsDefaults.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkTracker.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPomodoroSettingsDefaults : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "UPDATE UserSettings SET PomodoroWorkMinutes = 25, PomodoroShortBreakMinutes = 5, PomodoroLongBreakMinutes = 15, PomodoroRoundsBeforeLongBreak = 4 " +
+                "WHERE PomodoroWorkMinutes = 0 AND PomodoroShortBreakMinutes = 0 AND PomodoroLongBreakMinutes = 0 AND PomodoroRoundsBeforeLongBreak = 0;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
